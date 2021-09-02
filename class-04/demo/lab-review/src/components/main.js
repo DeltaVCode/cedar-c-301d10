10f53c9e7ccda91dca2e51f86166dcbe0ddc8eec
Keith Dahlby <dahlbyk@gmail.com>
2021-09-02 14:17:48 -0500

    Pull value out of dropdown

--- class-04/demo/lab-review/src/components/main.js
+++ class-04/demo/lab-review/src/components/main.js
@@ -1,24 +1,34 @@
 import React from 'react';
 // again, an inefficient import
-import { CardColumns, Container, Form } from 'react-bootstrap';
+import { Button, CardColumns, Container, Form } from 'react-bootstrap';
 import HornedBeast from './hornedBeast'
 
 export default class Main extends React.Component {
+  handleSubmit = (event) => {
+    // prevent actually submitting, just like in 201
+    event.preventDefault();
+
+    console.log(event.target.elements.hornCount.value);
+  }
+
   render() {
     let beasts = this.props.beasts;
     console.log(beasts);
 
     return (
       <Container as="main">
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group>
             <Form.Label>How many horns?</Form.Label>
-            <Form.Control as="select">
+            <Form.Control as="select" name="hornCount">
               <option value="">All</option>
               <option value="1">One</option>
               <option value="2">Two</option>
               <option value="3">Three</option>
             </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Button type="submit">Filter</Button>
           </Form.Group>
         </Form>
         <CardColumns>
