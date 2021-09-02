2d217a73e212d31c3486e7ef42996b178d6a5f1e
Keith Dahlby <dahlbyk@gmail.com>
2021-09-02 14:01:28 -0500

    Wrap main in a Fragment (<>)

--- class-04/demo/lab-review/src/components/main.js
+++ class-04/demo/lab-review/src/components/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // again, an inefficient import
-import { CardColumns } from 'react-bootstrap';
+import { CardColumns, Form } from 'react-bootstrap';
 import HornedBeast from './hornedBeast'
 
 export default class Main extends React.Component {
@@ -9,18 +9,20 @@ export default class Main extends React.Component {
     console.log(beasts);
 
     return (
-      <CardColumns>
-        {beasts.map((beast, i) => (
-          <HornedBeast
-            key={i}
-            beastIndex={i}
-            displayModalForIndex={this.props.handleSelectBeast}
-            title={beast.title}
-            imageUrl={beast.image_url}
-            description={beast.description}
-          />
-        ))}
-      </CardColumns>
+      <>
+        <CardColumns>
+          {beasts.map((beast, i) => (
+            <HornedBeast
+              key={i}
+              beastIndex={i}
+              displayModalForIndex={this.props.handleSelectBeast}
+              title={beast.title}
+              imageUrl={beast.image_url}
+              description={beast.description}
+            />
+          ))}
+        </CardColumns>
+      </>
     )
   }
 }
