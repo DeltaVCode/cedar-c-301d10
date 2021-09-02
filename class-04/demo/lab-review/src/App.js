ba7ae172fe528ed00730332bc63c624f5f0da5b9
Keith Dahlby <dahlbyk@gmail.com>
2021-09-02 11:47:32 -0500

    Add showModal state to control SelectedBeast

--- class-04/demo/lab-review/src/App.js
+++ class-04/demo/lab-review/src/App.js
@@ -9,6 +9,14 @@ import beastData from './data/data.json';
 // Converted from function component to a class component
 // by calling the function 'render' and putting it in a class that extends Component
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showModal: true,
+    };
+  }
+
   render() {
     let theme = 'dark';
 
@@ -17,7 +25,9 @@ class App extends React.Component {
         <Header theme={theme} />
         <Main beasts={beastData} />
         <Footer theme={theme} />
-        <SelectedBeast />
+        <SelectedBeast
+          show={this.state.showModal}
+        />
       </div>
     );
   }
--- class-04/demo/lab-review/src/components/selectedBeast.js
+++ class-04/demo/lab-review/src/components/selectedBeast.js
@@ -4,7 +4,7 @@ import { Modal } from 'react-bootstrap';
 class SelectedBeast extends React.Component {
   render() {
     return (
-      <Modal show={true}>
+      <Modal show={this.props.show}>
         <Modal.Dialog>
           <Modal.Header closeButton>
             <Modal.Title>Test!</Modal.Title>
