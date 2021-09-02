50d38742ac67d58c2f274a40f2f4216b7aea0337
Keith Dahlby <dahlbyk@gmail.com>
2021-09-02 12:29:45 -0500

    Show actual selected beast

--- class-04/demo/lab-review/src/components/selectedBeast.js
+++ class-04/demo/lab-review/src/components/selectedBeast.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Modal } from 'react-bootstrap';
+import { Button, Card, Modal } from 'react-bootstrap';
 
 class SelectedBeast extends React.Component {
   render() {
@@ -13,6 +13,25 @@ class SelectedBeast extends React.Component {
           <Modal.Header closeButton>
             <Modal.Title>{beast.title}</Modal.Title>
           </Modal.Header>
+          <Modal.Body>
+            <Card
+              bg="dark"
+              text="light"
+              onClick={this.displayAsModal}
+            >
+              <Card.Img variant="top" alt="" src={beast.image_url} />
+              <Card.Body>
+                <Card.Title>{beast.title}</Card.Title>
+                <Card.Text>
+                  {beast.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={this.props.handleClose} variant="secondary">Close</Button>
+          </Modal.Footer>
         </Modal.Dialog>
       </Modal>
     )
