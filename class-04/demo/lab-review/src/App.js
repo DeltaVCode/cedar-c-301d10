8d0e822159505df06715a4206b71b94876d0f85d
Keith Dahlby <dahlbyk@gmail.com>
2021-09-02 11:40:04 -0500

    Convert App to be a class component

--- class-04/demo/lab-review/src/App.js
+++ class-04/demo/lab-review/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Footer from './components/footer';
 import Header from './components/header';
@@ -5,17 +6,21 @@ import Main from './components/main';
 import SelectedBeast from './components/selectedBeast';
 import beastData from './data/data.json';
 
-function App() {
-  let theme = 'dark';
+// Converted from function component to a class component
+// by calling the function 'render' and putting it in a class that extends Component
+class App extends React.Component {
+  render() {
+    let theme = 'dark';
 
-  return (
-    <div className="App">
-      <Header theme={theme} />
-      <Main beasts={beastData} />
-      <Footer theme={theme} />
-      <SelectedBeast />
-    </div>
-  );
+    return (
+      <div className="App">
+        <Header theme={theme} />
+        <Main beasts={beastData} />
+        <Footer theme={theme} />
+        <SelectedBeast />
+      </div>
+    );
+  }
 }
 
 export default App;
