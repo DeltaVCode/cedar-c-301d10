62b3355a24cf312d4a38d6c891b98ae4208e7705
Keith Dahlby <dahlbyk@gmail.com>
2021-09-03 11:33:32 -0500

    Stick search in state

--- class-05/demo/city-explorer-react/src/components/main.js
+++ class-05/demo/city-explorer-react/src/components/main.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: null,
+    };
+  }
+
   handleLocationSearch = submitEvent => {
     submitEvent.preventDefault();
 
     console.log('submitted!', submitEvent.target);
+    let form = submitEvent.target;
+    let input = form.elements.search;
+    let search = input.value; // value from the form
+    this.setState({
+      // search: search,
+      search,
+    });
   }
 
   render() {
@@ -20,6 +35,10 @@ class Main extends React.Component {
             <button type="submit">Search</button>
           </div>
         </form>
+
+        <div>
+          Searched location is {this.state.search}
+        </div>
       </main>
     )
   }
