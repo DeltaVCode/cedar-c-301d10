63bddc47389099a3973855da3a4eef94e8d0814c
Keith Dahlby <dahlbyk@gmail.com>
2021-09-03 11:27:35 -0500

    Add a form with logging submit handler

--- class-05/demo/city-explorer-react/src/components/main.js
+++ class-05/demo/city-explorer-react/src/components/main.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 class Main extends React.Component {
+  handleLocationSearch = submitEvent => {
+    submitEvent.preventDefault();
+
+    console.log('submitted!', submitEvent.target);
+  }
+
   render() {
     return (
       <main>
-        hi
+        <form onSubmit={this.handleLocationSearch}>
+          <label>
+            Search for a location:
+            {' '} {/* add a space between */}
+            <input type="text" name="search" placeholder="Location" />
+          </label>
+          <div>
+            <button type="submit">Search</button>
+          </div>
+        </form>
       </main>
     )
   }
