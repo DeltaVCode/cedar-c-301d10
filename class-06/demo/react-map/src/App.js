1eb76faf181000a3f7303a3e23c348934ae913db
Keith Dahlby <dahlbyk@gmail.com>
2021-09-07 11:33:43 -0500

    Add handleSearch

--- class-06/demo/react-map/src/App.js
+++ class-06/demo/react-map/src/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  handleSearch = event => {
+    // avoid making new GET request
+    event.preventDefault();
+
+    let form = event.target;
+    let input = form.elements.search;
+    let q = input.value;
+    console.log(q);
+  };
+
   render() {
     return (
       <div className="App">
-        <form>
+        <form onSubmit={this.handleSearch}>
           <label>
             Search for a location:
             {' '} {/* add a space between */}
