5ae73f5a4fae3e4ddcb3665b5cf70dabbb8cab34
Keith Dahlby <dahlbyk@gmail.com>
2021-09-08 11:52:26 -0500

    Enable CORS on our server

--- class-07/demo/server/server.js
+++ class-07/demo/server/server.js
@@ -6,6 +6,22 @@ const express = require('express');
 // Constructor-ish for a new Express app
 const app = express();
 
+// Enable CORS to use from React
+const cors = require('cors');
+app.use(cors()); // "middleware"
+
+
+// Pass home callback function
+app.get('/', (request, response) => {
+  response.send('no place like home');
+});
+
+const items = ['apple', 'banana'];
+app.get('/shoppingList', (request, response) => {
+  // send items in response as application/json
+  response.json(items);
+});
+
 
 // Figure out what PORT to listen on
 const PORT = 3001;
