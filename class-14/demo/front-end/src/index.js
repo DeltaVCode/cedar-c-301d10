abb503435290f501bfec2867d3089c4504f53829
Keith Dahlby <dahlbyk@gmail.com>
2021-09-16 10:31:54 -0500

    Provide Auth0 context to our <App />

--- class-14/demo/front-end/src/index.js
+++ class-14/demo/front-end/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-txxoephp.us.auth0.com"
+      clientId="YnQ55inNLqxYMUkyD2ZhUFGZH9eRfgzS"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
